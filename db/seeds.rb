dcc6cf7f64bed3e15ba58735b57a7e1e70b79616
Marcin <marcin.kopanski@gmail.com>
2017-09-12 19:36:01 +0000

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,16 +34,18 @@ end
 puts "5 skills created"
 
 8.times do |portfolio|
-    Portfolio.create!(
-        title: "Portfolio title #{portfolio}",
-        subtitle: "Ruby on Rails",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        main_image: "https://placehold.it/600x400",
-        thumb_image: "https://placehold.it/350x200"
-    )
+   Portfolio.create!(
+      title: "Portfolio title #{portfolio}",
+      subtitle: "Ruby on Rails",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      main_image: "https://placehold.it/600x400",
+      thumb_image: "https://placehold.it/350x200"
+   )
+   
+   Portfolio.last.technologies.create!(
+      name: "Ruby"
+   )
 end
-
-puts "5 skills created"
 
 1.times do |portfolio|
     Portfolio.create!(
@@ -56,3 +58,12 @@ puts "5 skills created"
 end
 
 puts "9 portfolios created"
+
+3.times do |technology|
+   Technology.create!(
+      name: "Technology #{technology}",
+      portfolio_id: Portfolio.last.id
+   )
+end
+
+puts "3 technologies created"
