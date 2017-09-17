4076fa6d73e3191da71bb074410788bed690dbdc
Marcin <marcin.kopanski@gmail.com>
2017-09-17 19:53:20 +0000

    Added nav bar and defult heder cntent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContentConcern
   
   protect_from_forgery with: :exception
-  
 end
