02b743a4b60c3a8ee78c40986de4584a24e6bbe3
Marcin <marcin.kopanski@gmail.com>
2017-09-29 14:05:07 +0000

    Implemented twitter call on tech-news page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(email: "mk@test.pl", name: "Marcin Kopański", password: "123456", password_confirmation: "123456", roles: "site_admin")
+
 3.times do |topic|
    Topic.create!(
       title: "Topic #{topic}"   
