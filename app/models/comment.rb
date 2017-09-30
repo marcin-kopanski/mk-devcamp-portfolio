6937e7c83abd37bf5efe9057edcc9b7edf953f88
Marcin <marcin.kopanski@gmail.com>
2017-09-30 16:51:34 +0000

    refactored form into partial for comments and added validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :blog
+   validates_presence_of :content, presence: true, length: { minimum: 5, maximum: 1000 }
+   
+   belongs_to :user
+   belongs_to :blog
 end
