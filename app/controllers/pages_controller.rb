fd803f08dca217b08d40a8c881fa6ced4160246b
Marcin <marcin.kopanski@gmail.com>
2017-10-03 18:43:51 +0000

    Added view component for skills

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
    def home
       @posts = Blog.all
-      @skills = Skill.all
    end
    
    def about
+      @skills = Skill.all
    end
    
    def contact
